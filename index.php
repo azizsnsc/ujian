bec204866c942aee091ac333a7dd6a9df12d591d
iwansafr <iwansafr@gmail.com>
2020-03-23 07:58:49 +0700

    timezone

--- index.php
+++ index.php
@@ -53,6 +53,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
+	date_default_timezone_set("Asia/Bangkok");
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
